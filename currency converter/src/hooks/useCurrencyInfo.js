edd10bab1ecd95f325642340e3ba64e642e1a681
Masuk Khan <masukkhanemail@gmail.com>
2025-08-25 14:08:39 +0530

    learn to create custom hooks , useID hook by crating currency converter project

---
+++ currency converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+
+
+function useCurrencyInfo(date,currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+   ` https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${date}/v1/currencies/${currency}.json `   )
+      .then((res) => res.json()) 
+      .then((res) => setData(res[currency]));
+      console.log(data)
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
